07ff45bd4e29814d3a3d336d08c03824b5b36fbe
Sam Prakash Royyala <samprakash1410@gmail.com>
2024-11-15 11:24:32 +0530

    modified

--- ntire/network.tf
+++ ntire/network.tf
@@ -2,9 +2,9 @@ resource "aws_vpc" "primary_network" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "ntire"
+    Name = "ntire_dummy"
     Env = "Dev"
     createBy = "terraform" 
-    
+
   }
 }
