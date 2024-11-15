808c9b29e076775a629655f9354358ef222ac78f
Sam Prakash Royyala <samprakash1410@gmail.com>
2024-11-15 16:10:46 +0530

    added

--- ntire/inputs.tf
+++ ntire/inputs.tf
@@ -1,27 +1,37 @@
 variable "region" {
-  type = string
-  default = "us-east-1" 
+  type        = string
+  default     = "us-east-1"
   description = "region in which resource will be created"
 
 }
 
 variable "vpc_cidr" {
-  type = string
-  default = "10.100.0.0/16" 
+  type    = string
+  default = "10.100.0.0/16"
+}
+
+variable "subnetcount" {
+  type = number 
+  default = 3
+}
+
+variable "subnet_cidr" {
+  type = list(string)
+  default = [ "10.100.0.0/24","10.100.1.0/24","10.100.2.0/24" ]
 }
 
 variable "web_cidr" {
-  type = string
+  type    = string
   default = "10.100.0.0/24"
 }
 
 variable "business_cidr" {
-  type = string
+  type    = string
   default = "10.100.1.0/24"
 }
 
 variable "data_cidr" {
-  type = string
+  type    = string
   default = "10.100.2.0/24"
 }
 
