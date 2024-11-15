db27b5b9a37270b3f27cd53f5e9956054c345430
Sam Prakash Royyala <samprakash1410@gmail.com>
2024-11-15 11:54:07 +0530

    added web

--- ntire/network.tf
+++ ntire/network.tf
@@ -8,11 +8,11 @@ resource "aws_vpc" "primary_network" {
 
 
 
-resource "aws_subnet" "dummy-1 web" {
+resource "aws_subnet" "web_primary-1" {
   cidr_block = "10.100.0.0/24"
   vpc_id     = aws_vpc.primary_network.id
   tags = {
-    Name = "dummy-1 web"
+    Name = "web_primary-1"
 
   }
 }
